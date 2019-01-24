be3e5fdcec832d04a6e2229e2422349445b7d8d7
Nick <hoop.bbb@gmail.com>
2019-01-24 14:19:00 -0800

    added gcc-multilib and apt update

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-apt install bash binutils bzip2 coreutils diffutils findutils gawk gcc git grep m4 make patch perl sed tar texinfo vim
+apt install bash binutils bzip2 coreutils diffutils findutils gawk gcc git grep m4 make patch perl sed tar texinfo vim gcc-multilib
+apt update
