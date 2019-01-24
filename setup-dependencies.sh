62de4fe2276f71f094f53134b00eca31c46f4e19
Nick <hoop.bbb@gmail.com>
2019-01-24 13:17:56 -0800

    added git

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt install bash binutils bzip2 coreutils diffutils findutils gawk gcc grep m4 make patch perl sed tar texinfo 
+apt install bash binutils bzip2 coreutils diffutils findutils gawk gcc git grep m4 make patch perl sed tar texinfo 
