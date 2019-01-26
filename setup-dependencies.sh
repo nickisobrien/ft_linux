45e231be977f1f3c10d3cfc8ab55bfc60e2b8373
root <root@ip-172-31-29-172.us-west-2.compute.internal>
2019-01-26 23:56:03 +0000

    added bison

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt install bash binutils bzip2 coreutils diffutils findutils gawk gcc git grep m4 make patch perl sed tar texinfo vim gcc-multilib
-apt update
+yum install bash binutils bison bzip2 coreutils diffutils findutils gawk gcc git grep m4 make patch perl sed tar texinfo vim gcc-multilib
+yum update
