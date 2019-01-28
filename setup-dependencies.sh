5fff420f2984db4cdbee95b2c4bf22c6addf0b54
root <root@ip-172-31-29-172.us-west-2.compute.internal>
2019-01-28 21:52:36 +0000

    Added needed dependencies as went

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 yum install bash binutils bison bzip2 coreutils diffutils findutils gawk gcc git grep m4 make patch perl sed tar texinfo vim gcc-multilib
+yum groupinstall 'Development Tools'
 yum update
